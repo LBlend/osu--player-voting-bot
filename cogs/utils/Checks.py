62214c787a4857ecbacd9a284894a252ffd39124
LBlend <24893890+LBlend@users.noreply.github.com>
2019-10-13 16:04:29 +0200

    removed unnecessary print statement

--- cogs/utils/Checks.py
+++ cogs/utils/Checks.py
@@ -4,6 +4,5 @@ from discord.ext import commands
 def is_guild_member():
     def predicate(ctx):
         guild = ctx.bot.get_guild(565230012366848000)
-        print(guild.id)
         return ctx.author in guild.members
     return commands.check(predicate)
